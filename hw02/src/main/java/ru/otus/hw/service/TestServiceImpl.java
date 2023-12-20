9c446e10cf0f5ce8cf4d173206ab98a6da75b737
Sitnikov Vladimir <combasoft@yandex.ru>
2023-12-20 21:21:42 +0400

    Using java annotations for spring context configuration

---
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -0,0 +1,47 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.domain.Student;
+import ru.otus.hw.domain.TestResult;
+
+@Service
+@RequiredArgsConstructor
+public class TestServiceImpl implements TestService {
+
+    private final IOService ioService;
+
+    private final QuestionDao questionDao;
+
+    private final QuestionPresenterService presenterService;
+
+    @Override
+    public TestResult executeTestFor(Student student) {
+        ioService.printLine("");
+        ioService.printFormattedLine("Please answer the questions below%n");
+        var questions = questionDao.findAll();
+        var testResult = new TestResult(student);
+
+        for (Question question : questions) {
+            var isAnswerValid = askQuestionAndGetAnswer(question); // Задать вопрос, получить ответ
+            testResult.applyAnswer(question, isAnswerValid);
+        }
+        return testResult;
+    }
+
+    private boolean askQuestionAndGetAnswer(Question question) {
+        final int lowerBound = 1;
+        int upperBound = question.answers().size();
+        ioService.printLine(presenterService.getPresentation(question));
+        int userChoice = ioService.readIntForRangeWithPrompt(lowerBound,
+                upperBound,
+                "Please choose answer number(it should be in range [" + lowerBound + ".." + upperBound + "] ): ",
+                "Wrong number format or number is out of bounds [1.." + upperBound + "]");
+        ioService.printLine("");
+        Answer answer = question.answers().get(userChoice - lowerBound);
+        return answer.isCorrect();
+    }
+}
