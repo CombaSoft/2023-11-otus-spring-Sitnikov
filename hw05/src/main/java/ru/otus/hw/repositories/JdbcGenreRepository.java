b3ac53dc5cc62a84840a66555525ded68a08f0a8
Sitnikov Vladimir <combasoft@yandex.ru>
2024-01-13 17:47:30 +0400

    Update imports

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
-import ru.otus.hw.models.Author;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 
 @Repository
 @RequiredArgsConstructor
